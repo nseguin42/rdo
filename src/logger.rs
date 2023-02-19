3a1f7952dbb89b34e91a1351af0cca788c220bcf
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-02-19 15:54:35 -0600

    use pretty_env_logger add cargo husky

--- src/logger.rs
+++ src/logger.rs
@@ -1,25 +1,14 @@
-use std::io::Write;
+
 
 use config::Config;
-use pretty_env_logger::env_logger;
 
-
-
+use pretty_env_logger::{formatted_builder};
 
 pub fn setup_logger(config: Config) {
     let log_level = config.get_string("log.level");
 
-    env_logger::Builder::from_default_env()
+    formatted_builder()
         .filter_level(log_level.unwrap().parse().unwrap())
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "[{}][{}] {}",
-                record.level(),
-                record.target(),
-                record.args()
-            )
-        })
         .init();
 
     info!("Logger initialized");
