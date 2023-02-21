7d5d007c73466df3f3dbc1d007bb7695b4e5f386
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-02-20 21:44:10 -0600

    more cleanup

--- src/task_queue.rs
+++ src/task_queue.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::hash::Hash;
 
@@ -6,6 +6,7 @@ use petgraph::graph::DiGraph;
 use petgraph::prelude::NodeIndex;
 use petgraph::visit::{NodeFiltered, Topo, Walker};
 
+use crate::error::Error;
 use crate::runnable::Runnable;
 use crate::task::Task;
 
@@ -41,72 +42,99 @@ where
 
 impl<'a, T, K> GraphBinding<'a, T, K>
 where
-    T: GraphLike<'a, K> + PartialEq + Eq + 'a,
+    T: GraphLike<'a, K> + PartialEq + Eq + Debug + 'a,
     K: PartialEq + Eq + Hash + Debug,
 {
-    pub fn new(nodes: Vec<&'a T>) -> GraphBinding<'a, T, K> {
+    pub fn new(nodes: Vec<&'a T>) -> Result<GraphBinding<'a, T, K>, Error> {
         let graph = DiGraph::new();
         let key_to_id = HashMap::new();
         let mut graph_binding = GraphBinding { graph, key_to_id };
-        graph_binding.add_nodes(nodes);
-        graph_binding
+        graph_binding.add_nodes(nodes)?;
+        Ok(graph_binding)
     }
 
     fn add_node(&mut self, node: &'a T) {
         let id = self.graph.add_node(node);
-        self.key_to_id.insert(node.get_key(), id);
+        let result = self.key_to_id.insert(node.get_key(), id);
+        if result.is_some() {
+            panic!("Duplicate key found: {:?}", node.get_key())
+        }
     }
 
-    fn add_nodes(&mut self, nodes: Vec<&'a T>) {
+    fn add_nodes(&mut self, nodes: Vec<&'a T>) -> Result<(), Error> {
         for &node in nodes.iter() {
             self.add_node(node);
         }
-        self.add_child_edges(nodes);
+        self.add_child_edges(nodes)
     }
 
-    fn add_child_edges(&mut self, nodes: Vec<&'a T>) {
+    fn add_child_edges(&mut self, nodes: Vec<&'a T>) -> Result<(), Error> {
         for node in nodes {
-            let node_id = self.key_to_id.get(node.get_key()).unwrap();
+            let node_id = self.find_node_id_by_key(node.get_key())?;
             for child_key in node.get_children_keys() {
-                dbg!(&child_key);
-                let child_id = self.key_to_id.get(child_key).unwrap();
-                self.graph.add_edge(*child_id, *node_id, ());
+                let child_id = self.find_node_id_by_key(child_key)?;
+                self.graph.add_edge(child_id, node_id, ());
             }
         }
+
+        Ok(())
     }
 
-    fn get_transitive_closure(&'a self, nodes: Vec<&'a T>) -> HashSet<&NodeIndex> {
+    fn find_node_id_by_key(&self, key: &K) -> Result<NodeIndex, Error> {
+        let result = self.key_to_id.get(key);
+        match result {
+            Some(id) => Ok(*id),
+            None => Err(Error::Unspecified(format!(
+                "Could not find node with key {:?}",
+                key
+            ))),
+        }
+    }
+
+    fn get_transitive_closure(&self, nodes: Vec<&'a T>) -> Result<HashSet<NodeIndex>, Error> {
         let mut closure = HashSet::new();
-        let mut queue = VecDeque::new();
+        let mut queue = Vec::new();
 
         for node in nodes {
-            queue.push_back(node);
+            queue.push(node);
         }
 
-        while let Some(node) = queue.pop_front() {
+        while let Some(node) = queue.pop() {
+            let node_id = self.find_node_id_by_key(node.get_key())?;
+            if !closure.insert(node_id) {
+                continue;
+            }
+
             for child_key in node.get_children_keys() {
-                let child_id = self.key_to_id.get(child_key).unwrap();
+                let child_id = self.find_node_id_by_key(child_key)?;
                 if !closure.insert(child_id) {
                     continue;
                 }
-                queue.push_back(self.graph.node_weight(*child_id).unwrap());
+                queue.push(
+                    self.graph
+                        .node_weight(child_id)
+                        .unwrap_or_else(|| panic!("No node at id: {:?}", child_id)),
+                );
             }
         }
 
-        closure
+        Ok(closure)
     }
 
-    pub fn get_all_nodes(&'a self) -> Vec<&'a T> {
+    pub fn get_all_nodes(&self) -> Vec<&'a T> {
         self.graph.node_weights().copied().collect()
     }
 
     pub fn topological_sort(&'a self, nodes: Vec<&'a T>) -> impl Iterator<Item = &'a T> + 'a {
-        let node_ids = self.get_transitive_closure(nodes);
+        let node_ids = self.get_transitive_closure(nodes).unwrap();
         let node_filter_fn = |id: NodeIndex| node_ids.contains(&id);
 
         let filtered = NodeFiltered::from_fn(&self.graph, node_filter_fn);
-        Topo::new(&filtered)
-            .iter(&self.graph)
-            .map(|id| *self.graph.node_weight(id).unwrap())
+        Topo::new(&filtered).iter(&self.graph).map(|id| {
+            *self
+                .graph
+                .node_weight(id)
+                .unwrap_or_else(|| panic!("No node at id: {:?}", id))
+        })
     }
 }
